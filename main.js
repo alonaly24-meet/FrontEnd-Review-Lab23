78eb7cde5887b523587ecab39a7c10d36ba6d5bc
alonaly24-meet <alona.lynfield@gmail.com>
2023-07-17 18:27:24 +0300

    fun

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 function changeBackgroundColor(color) {
 
-	console.log(color);
-	document.getElementsByTagName("body")[0].style.backgroundColor= color;
+	var colors = ["yellow", "blue"]; // Array of colors to switch between
+  var body = document.getElementsByTagName("body")[0];
+  var currentColor = body.style.backgroundColor;
+
+  if (currentColor === colors[0]) {
+    body.style.backgroundColor = colors[1];
+  } else {
+    body.style.backgroundColor = colors[0];
+  }
 }
